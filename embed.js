9ae56b08c79186136be1e4494b9a12c819856b18
Guimz <brunopereira13@hotmail.co.uk>
2025-05-28 13:49:04 +0100

    hide overflow of chatbot

--- embed.js
+++ embed.js
@@ -100,7 +100,7 @@
                 <!-- Chat Window -->
                 <div id="chat-window" class="hidden w-96 h-[600px] bg-white rounded-b-lg shadow-lg flex flex-col">
                     <!-- Chat Header -->
-                    <div class="p-4 rounded-t-lg flex items-center justify-between" style="background-color: ${window.echoConfig.primaryColor}">
+                    <div class="p-4 rounded-t-lg overflow-hidden flex items-center justify-between" style="background-color: ${window.echoConfig.primaryColor}">
                         <div class="flex items-center gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="${window.echoConfig.textColor}">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
